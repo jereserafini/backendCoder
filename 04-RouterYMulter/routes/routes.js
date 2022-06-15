7575299d1593c325a91919456609d06ebbbd3b94
jereserafini <jerefernandez98@gmail.com>
2022-06-15 19:34:07 -0300

    Router y multer corregido

--- 04-RouterYMulter/routes/routes.js
+++ 04-RouterYMulter/routes/routes.js
@@ -1,73 +1,104 @@
 const { Router } = require("express");
 
-const route = Router()
+const route = Router();
 
-let products = []
+let products = [];
 
-route.get( '/products', (req, res) => {
-    res.json(products)
-})
+route.get("/products", (req, res) => {
+  try {
+    res.json(products);
+  } catch (error) {
+    res
+    .status(error.statusCode ? error.statusCode : 500)
+    .json({ error: error.message });
+  }
+});
 
-route.get( '/products/:id', (req, res) => {
-    const {id} = req.params
-
-    if (products.find(prod => prod.id == id)) {
-
-        const product = products.find( prod => prod.id == id)
-        res.json(product)
-    
+route.get("/products/:id", (req, res) => {
+  try {
+    const id = Number(req.params.id);
+  
+    if (products.find((prod) => prod.id === id)) {
+      const product = products.find((prod) => prod.id === id);
+      res.json(product);
     } else {
-        res.json(`El producto con el id: ${id} no existe`)
+      res.json(`El producto con el id: ${id} no existe`);
     }
-})
+    
+  } catch (error) {
+    res
+    .status(error.statusCode ? error.statusCode : 500)
+    .json({ error: error.message });
+  }
+});
 
-route.post( '/products', (req, res) => {
-    const { title, price, thumbnail } = req.body
-    let id
-
+route.post("/products", (req, res) => {
+  try {
+    const { title, price, thumbnail } = req.body;
+    let id;
+  
     if (products.length == 0) {
-        id = 1
+      id = 1;
     } else {
-        id = products.length + 1
+      id = products[products.length - 1].id + 1;
+    }
+  
+    products.push({ id, title, price, thumbnail });
+  
+    res.json({ id, title, price, thumbnail });
+
+  } catch (error) {
+    res
+    .status(error.statusCode ? error.statusCode : 500)
+    .json({ error: error.message });
+  }
+
+});
+
+route.put("/products/:id", (req, res) => {
+  try {
+    const id = Number(req.params.id);
+  
+    if (products.find((prod) => prod.id === id)) {
+      const { title, price, thumbnail } = req.body;
+  
+      products.map((prod) => {
+        if (prod.id === id) {
+          (prod.title = title),
+            (prod.price = price),
+            (prod.thumbnail = thumbnail);
+        }
+      });
+  
+      res.json(products);
+
+    } else {
+      res.json(`El producto con el id: ${id} no existe`);
     }
 
-    products.push({ id, title, price, thumbnail })
+  } catch (error) {
+    res
+    .status(error.statusCode ? error.statusCode : 500)
+    .json({ error: error.message });
+  }
+});
 
-    res.json({ id, title, price, thumbnail })
+route.delete("/products/:id", (req, res) => {
+  try {
+    const id = Number(req.params.id);
 
-})
+    if (products.find((prod) => prod.id === id)) {
+      products = products.filter((prod) => prod.id !== id);
 
-route.put( '/products/:id', (req, res) => {
-    const {id} = req.params
-
-    if (products.find(prod => prod.id == id)) {
-
-        const { title, price, thumbnail } = req.body
-
-        products = products.filter(prod => prod.id != id)
-
-        products.push({ id, title, price, thumbnail })
-
-        res.json(products)
-    
+      res.json(products);
     } else {
-        res.json(`El producto con el id: ${id} no existe`)
+      res.json(`El producto con el id: ${id} no existe`);
     }
+  } catch (error) {
+    res
+      .status(error.statusCode ? error.statusCode : 500)
+      .json({ error: error.message });
+  }
+});
 
-})
-
-route.delete( '/products/:id', (req, res) => {
-    const {id} = req.params
-
-    if (products.find(prod => prod.id == id)) {
-
-        products = products.filter(prod => prod.id != id)
-    
-        res.json(products)
-    
-    } else {
-        res.json(`El producto con el id: ${id} no existe`)
-    }
-})
-
-module.exports = route
+module.exports = route;
--- 04-RouterYMulter/server.js
+++ 04-RouterYMulter/server.js
@@ -1,20 +1,24 @@
-const express = require('express')
-const route = require('./routes/routes')
+const express = require("express");
+const route = require("./routes/routes");
 
-const app = express()
-const PORT = 8080
+const app = express();
+const PORT = 8080;
 
 //Middleware necesario para tomar datos del body
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Middleware para recivir archivos estaticos, el dirname es para llamar ruta absoluta
-app.use('/', express.static(__dirname + '/public'))
+app.use("/", express.static(__dirname + "/public"));
 
 //Llamado a las rutas
-app.use( '/api', route )
+app.use("/api", route);
 
 //Escuchar en el puerto 8080
-app.listen( PORT, () => {
-    console.log(`Servidor escuchando en el puerto: ${PORT}`)
-} )
+app.listen(PORT, (error) => {
+  try {
+    console.log(`Servidor escuchando en el puerto: ${PORT}`);
+  } catch {
+    console.log(error);
+  }
+});
