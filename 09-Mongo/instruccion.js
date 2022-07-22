94cfe3bfb9bdb76c966ff5ff728dbca310b16fb3
jereserafini <jerefernandez98@gmail.com>
2022-07-22 20:32:39 -0300

    Base de datos con mongo

--- 09-Mongo/instruccion.js
+++ 09-Mongo/instruccion.js
@@ -1,25 +1,39 @@
-[
-    { id: 1,title:"Remera",price:2500,url:"img1"},
-    { id: 2,title:"gorra",price:1500,url:"img2"},
-    { id: 3,title:"zapatilla",price:5000,url:"img3"},
-    { id: 4,title:"buzo",price:4500,url:"img4"},
-    { id: 5,title:"media",price:500,url:"img5"},
-    { id: 6,title:"campera",price:5000,url:"img6"},
-    { id: 7,title:"short",price:1000,url:"img7"},
-    { id: 8,title:"jean",price:4500,url:"img8"},
-    { id: 9,title:"boxer",price:700,url:"img9"},
-    { id: 10,"title":"ojotas",price:1200,url:"img10"},
-]
+db.products.insert([{title:"Remera",price:2500,url:"img1"},{title:"gorra",price:1500,url:"img2"},{title:"zapatilla",price:5000,url:"img3"},{title:"buzo",price:4500,url:"img4"},{title:"media",price:500,url:"img5"},{title:"campera",price:5000,url:"img6"},{title:"short",price:1000,url:"img7"},{title:"jean",price:4500,url:"img8"},{title:"boxer",price:700,url:"img9"},{title:"ojotas",price:1200,url:"img10"}])
+db.chats.insert([{email:"test1@test.com",message:"test",date:"test"},{email:"test2@test.com",message:"test",date:"test"},{email:"test3@test.com",message:"test",date:"test"},{email:"test4@test.com",message:"test",date:"test"},{email:"test5@test.com",message:"test",date:"test"},{email:"test6@test.com",message:"test",date:"test"},{email:"test7@test.com",message:"test",date:"test"},{email:"test8@test.com",message:"test",date:"test"},{email:"test9@test.com",message:"test",date:"test"},{email:"test10@test.com",message:"test",date:"test"}])
 
-[
-    {id:1,email:"test1@test.com",message:"test",date:"test"},
-    {id:2,email:"test2@test.com",message:"test",date:"test"},
-    {id:3,email:"test3@test.com",message:"test",date:"test"},
-    {id:4,email:"test4@test.com",message:"test",date:"test"},
-    {id:5,email:"test5@test.com",message:"test",date:"test"},
-    {id:6,email:"test6@test.com",message:"test",date:"test"},
-    {id:7,email:"test7@test.com",message:"test",date:"test"},
-    {id:8,email:"test8@test.com",message:"test",date:"test"},
-    {id:9,email:"test9@test.com",message:"test",date:"test"},
-    {id:10,email:"test10@test.com",message:"test",date:"test"},
-]
+
+db.products.insertOne({title:"Sandalia",price:1250,url:"img11"})
+db.chats.insertOne({email:"test11@test.com",message:"test",date:"test"})
+
+db.chats.find()
+db.products.find()
+
+db.chats.estimatedDocumentCount()
+db.products.estimatedDocumentCount()
+
+db.products.find({price: {$lt: 1000}})
+
+db.products.find({$and: [{price: {$gte: 1000}}, {price: {$lte: 3000}}]})
+
+db.products.find({price: {$gt: 3000}})
+
+db.products.find({title: 1}).sort({price: 1}).limit(1).skip(2)
+
+db.products.update({},{$set: {stock: 100}},{upsert:false,multi:true})
+
+db.products.update({price: {$gt: 4000}},{$set: {stock: 0}},{upsert:false,multi:true})
+
+db.products.deleteMany({price: {$lt: 1000}})
+
+db.createUser(
+    {
+        user:"pepe",
+        pwd:"asd456",
+        roles: [
+            {
+                role:"read",
+                db:"ecommerce"
+            }
+        ]
+    }
+)
