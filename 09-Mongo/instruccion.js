1e97807279a7f96147d6e5015a1147d51296bafd
jereserafini <jerefernandez98@gmail.com>
2022-07-22 20:32:16 -0300

    Base de datos con mongo

---
+++ 09-Mongo/instruccion.js
@@ -0,0 +1,25 @@
+[
+    { id: 1,title:"Remera",price:2500,url:"img1"},
+    { id: 2,title:"gorra",price:1500,url:"img2"},
+    { id: 3,title:"zapatilla",price:5000,url:"img3"},
+    { id: 4,title:"buzo",price:4500,url:"img4"},
+    { id: 5,title:"media",price:500,url:"img5"},
+    { id: 6,title:"campera",price:5000,url:"img6"},
+    { id: 7,title:"short",price:1000,url:"img7"},
+    { id: 8,title:"jean",price:4500,url:"img8"},
+    { id: 9,title:"boxer",price:700,url:"img9"},
+    { id: 10,"title":"ojotas",price:1200,url:"img10"},
+]
+
+[
+    {id:1,email:"test1@test.com",message:"test",date:"test"},
+    {id:2,email:"test2@test.com",message:"test",date:"test"},
+    {id:3,email:"test3@test.com",message:"test",date:"test"},
+    {id:4,email:"test4@test.com",message:"test",date:"test"},
+    {id:5,email:"test5@test.com",message:"test",date:"test"},
+    {id:6,email:"test6@test.com",message:"test",date:"test"},
+    {id:7,email:"test7@test.com",message:"test",date:"test"},
+    {id:8,email:"test8@test.com",message:"test",date:"test"},
+    {id:9,email:"test9@test.com",message:"test",date:"test"},
+    {id:10,email:"test10@test.com",message:"test",date:"test"},
+]
